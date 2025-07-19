cdb40fe3d78851093bfa7d788bcec754366e8d9b
Eduardo Barroso <dev@edbar.xyz>
2025-07-19 16:47:50 -0300

    feat: add logger to server

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"frontendmasters.com/reelingit/logger"
 )
 
 func main() {
+	logInstance, err := logger.NewLogger("movies.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger %v", err)
+	}
+	defer logInstance.Close()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":8080"
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed to initialize", err)
 	}
 }
